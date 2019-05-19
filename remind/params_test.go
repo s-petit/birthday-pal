73065603b1f924d87d9401221903ca0d0320ecbe
Stéphane Petit <spetit@enjoycode.fr>
2019-05-19 10:34:02 +0200

    Set current date at midnight UTC when comparing with birthdays

--- remind/params_test.go
+++ remind/params_test.go
@@ -9,8 +9,9 @@ import (
 
 func Test_should_calculate_remind_day(t *testing.T) {
 
-	currentDate := testdata.BirthDate(2018, time.August, 30)
+	currentDate := testdata.LocalDate(2018, time.August, 30)
 	remindParams := Params{Today: currentDate, InNbDays: 3}
 
-	assert.Equal(t, testdata.LocalDate(2018, time.September, 2), remindParams.RemindDay())
+	assert.Equal(t, testdata.BirthDate(2018, time.September, 2), remindParams.RemindDay())
+	assert.Equal(t, time.UTC, remindParams.RemindDay().Location())
 }
